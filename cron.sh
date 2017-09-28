44066bb3aff413e24ed269def2425a4fbf1c4af8
rootkit.ty <rootkit.ty@tuta.io>
2017-09-28 21:24:29 +1000

    Fixing cron notifications

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 date >> logs/git.log
 echo "Attempting to pull repo" >> logs/git.log
-git pull origin master >> logs/git.log
+git pull -q origin master >> logs/git.log
 
 date >> logs/hugo.log
 echo "Attempting to build HTML with Hugo" >> logs/hugo.log
